f854b5a4361769f2efc521156c7373f1268a2e82
Felix <felix.lapierre1@gmail.com>
2020-01-20 11:32:29 -0500

    Basic HTTP client GET request

---
+++ src/javahttpclient/JavaHttpClient.java
@@ -0,0 +1,51 @@
+package javahttpclient;
+import java.net.*;
+import java.io.*;
+/**
+ *
+ * @author Felix
+ */
+public class JavaHttpClient {
+    private Socket socket;
+    private InputStream in;
+    private OutputStream out;
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JavaHttpClient client = new JavaHttpClient("httpbin.org", 80);
+    }
+    
+    public JavaHttpClient(String address, int port) {
+        try {
+            socket = new Socket(address, port);
+            in = socket.getInputStream();
+            out = socket.getOutputStream();
+            
+            String request = "GET /status/418 HTTP/1.0\r\n\r\n";
+            
+            out.write(request.getBytes());
+            out.flush();
+            
+            StringBuilder response = new StringBuilder();
+            
+            int data = in.read();
+            
+            while(data != -1) {
+                response.append((char)data);
+                data = in.read();
+            }
+            
+            System.out.println(response);
+            socket.close();
+            
+            
+        } catch(UnknownHostException e) {
+            System.out.println(e);
+        } catch(IOException e) {
+            System.out.println(e);
+        }
+    }
+    
+}
